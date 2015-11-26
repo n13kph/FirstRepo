ec29166a7a9c348b687cf858567fcc310775dd0f
Your Name <youremail@ccny.cuny.edu>
2015-11-25 21:11:14 -0500

    This is the main program file

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //Copies and pastes a file already on my computer
+using std::cout; //Here we pull out one name from iostream file so we don't
+//have to write the "std::cout" we could just write cout.
+
+int main() //The main is usually where the program starts
+{
+	cout << "hello world. \n\n"; //This here outputs the text "hello world"
+	//the "\n" is an escape sequence. it creates a line spacer
+
+
+	return 0; //This exits main and thus shuts down the program.
+	//The return of value 0 back to the shell indicates it was a sucessful run
+
+
+}
